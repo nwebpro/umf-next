514b08e52e6401b68ad8831c4b6f7a20b72647e6
nwebpro <abnaeem.bsc@gmail.com>
2024-01-08 12:49:45 +0600

    configer ant design and create layout

---
+++ src/app/(withlayout)/student/page.tsx
@@ -0,0 +1,9 @@
+const Student = () => {
+	return (
+		<div>
+			<h1>This page is for student</h1>
+		</div>
+	);
+};
+
+export default Student;
---
+++ src/app/(withlayout)/super-admin/page.tsx
@@ -0,0 +1,10 @@
+
+const SuperAdmin = () => {
+	return (
+		<div>
+			<h1>This page is for Super Admin</h1>
+		</div>
+	);
+};
+
+export default SuperAdmin;
