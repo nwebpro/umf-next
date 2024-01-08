e88b42fdcd3bdc61270dd7c135928a4628ba6af3
nwebpro <abnaeem.bsc@gmail.com>
2024-01-08 16:38:01 +0600

    Sidebar display role wise implement

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,51 +1,46 @@
 'use client'
 import { useState } from 'react';
-import {
-	DesktopOutlined,
-	FileOutlined,
-	PieChartOutlined,
-	TeamOutlined,
-	UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
 import { Layout, Menu } from 'antd';
+import SidebarItems from '@/constants/SidebarItems';
+import { USER_ROLE } from '@/constants/role';
 
 const { Sider } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
-
-function getItem(
-	label: React.ReactNode,
-	key: React.Key,
-	icon?: React.ReactNode,
-	children?: MenuItem[],
-): MenuItem {
-	return {
-		key,
-		icon,
-		children,
-		label,
-	}
-}
-  
-const items: MenuItem[] = [
-	getItem('Option 1', '1', <PieChartOutlined />),
-	getItem('Option 2', '2', <DesktopOutlined />),
-	getItem('User', 'sub1', <UserOutlined />, [
-		getItem('Tom', '3'),
-		getItem('Bill', '4'),
-		getItem('Alex', '5'),
-	]),
-	getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-	getItem('Files', '9', <FileOutlined />),
-];
-
 const Sidebar = () => {
 	const [collapsed, setCollapsed] = useState(false);
+	const role = USER_ROLE.STUDENT;
+
 	return (
-		<Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-			<div className="demo-logo-vertical" />
-			<Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+		<Sider 
+			collapsible 
+			collapsed={collapsed} 
+			onCollapse={(value) => setCollapsed(value)}
+			width={280}
+			style={{
+				overflow: 'auto',
+				height: '100vh',
+				position: 'sticky',
+				top: 0,
+				left: 0,
+			}}
+		>
+			<div
+				style={{
+					color: 'white',
+					fontSize: '2rem',
+					textAlign: 'center',
+					fontWeight: 'bold',
+					margin: '1rem 0',
+				}}
+			>
+				University
+			</div>
+			<Menu 
+				theme="dark" 
+				defaultSelectedKeys={['1']} 
+				mode="inline" 
+				items={SidebarItems(role)}
+			/>
 		</Sider>
 	);
 };
